c767217b28a8d4ac17e9b899891a47a535317295
RogérioMB <69520729+rogerioMB-hub@users.noreply.github.com>
2024-12-20 16:43:42 -0300

    Update dac_01_test.py

--- Programas_micropython/rp_pi_pico/proj_dac_mcp4725_teste/dac_01_test.py
+++ Programas_micropython/rp_pi_pico/proj_dac_mcp4725_teste/dac_01_test.py
@@ -11,7 +11,6 @@ from mcp4725 import MCP4725, BUS_ADDRESS
 
 
 ADC0 = ADC(Pin(26))
-ADC1 = ADC(Pin(27))
 conv_factor = 3.3 / 65535
 
 # usando I2C hardware com pinos originais 9 e 8
@@ -20,22 +19,10 @@ i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=400000)
 #create the MCP4725 driver
 dac=MCP4725(i2c,BUS_ADDRESS[0])
 
-#criando led para pisca-pisac
-led1 = Pin(25, Pin.OUT)
-
 #criando oled
 oled_width = 128
 oled_height = 32
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
-
-def pisca(led, ms_, vezes):
-    for x in range(vezes):
-        led.value(1)
-        time.sleep_ms(ms_)
-        led.value(0)
-        time.sleep_ms(ms_)
-
-pisca(led1, 100, 5)
 
 for v in range(4096):
     #gera valor
@@ -48,12 +35,4 @@ for v in range(4096):
     oled.text("ADC1: ",5,8)
     oled.text(str(round(volts,2)),45,8)
     oled.show()
-
-pisca(led1, 100, 5)
-
     
-
-
-
-
-
